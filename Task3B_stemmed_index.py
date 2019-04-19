8ac05aa51927e14aba05ee6ec1a1d2548cdf19b3
Poorva Sonparote <sonparote.p@husky.neu.edu>
2019-04-19 07:48:33 -0400

    Task 3B - code. Yet to upload output files

--- Task3B_stemmed_index.py
+++ Task3B_stemmed_index.py
@@ -1,39 +1,61 @@
 import re
+import pdb
+import sys
+import traceback
 
 import corpusGeneration
 import nltk
 
 index = {}
-
-with open("C:\\Users\\Poorva\\Downloads\\test-collection\\cacm_stem.txt", "r") as file:
-
-    j = 0
-    text =""
-    lines = file.readlines()
-    for i in range(0, len(lines)):
-
-        line = lines[i]
-
-        if line[0] == "#":
-
-            j = i + 1
-
-            docid = line[2:]
-            print("DOCID : ", docid)
-
-            while "#" not in lines[j]:
-
-                text = text + lines[j]
-                j = j + 1
+inverted_index = {}
 
 
-            match = re.search(r'\sam|\spm', text)
+def main(*args, **kw):
 
-            if match:
-                print(text[:match.end() -2 ])
+    with open("C:\\Users\\Poorva\\Downloads\\test-collection\\cacm_stem.txt", "r") as file:
 
-            text = ""
+        j = 0
+        text =""
+        lines = file.readlines()
+        for i in range(0, len(lines)):
+
+            line = lines[i]
+
+            if line[0] == "#":
+
+                j = i + 1
+
+                docid = line[2:]
 
 
 
+                while "#" not in lines[j] :
+
+                        text = text + lines[j]
+                        j = j + 1
+
+
+                        if (j >= len(lines)):
+                           break;
+
+
+
+                match = re.search(r'\sam|\spm', text)
+
+                if match:
+
+                        index[int(docid.strip())] = text[:match.end() -2].split()
+
+                text = ""
+
+
+
+main()
+
+for key, value in index.items():
+    for v in value:
+        inverted_index.setdefault(v, []).append(key)
+
+for k, v in inverted_index.items():
+    print(k, ":", v)
 
